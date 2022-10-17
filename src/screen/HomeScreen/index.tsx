069790ed334e376abc70f3ad22085d6b3b46f4d7
PrajaktaChandilkar <prajakta0704@gamil.com>
2022-10-17 22:04:10 +0530

    update

---
+++ src/screen/HomeScreen/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import LeftPane from "./LeftPane";
+import RightPane from "./RightPane";
+import styled from "styled-components";
+
+const HomeScreenContainer = styled.div`
+  // display: grid;
+  // grid-template-columns: 1fr 1.5fr;
+  position:relative;
+  width: 100%;
+  height: 100vh;
+`;
+
+const HomeScreen = () => {
+  return (
+    <HomeScreenContainer>
+      <LeftPane />
+      <RightPane />
+    </HomeScreenContainer>
+  );
+};
+
+export default HomeScreen;
